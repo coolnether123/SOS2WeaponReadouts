8b07a6effac23995cbe0329bbb92b2002844d817
coolnether123 <32144172+coolnether123@users.noreply.github.com>
2026-08-07 16:18:39 -0500

    Close legacy metadata XML here-strings

--- Tools/SupportCascadeHooks.ps1
+++ Tools/SupportCascadeHooks.ps1
@@ -4,6 +4,7 @@ $repository = [System.IO.Path]::GetFullPath((Split-Path -Parent $PSScriptRoot));
 $aboutXml=@"
 <?xml version="1.0" encoding="utf-8"?>
 <ModMetaData><name>$name</name><author>CoolNether123</author><packageId>$package</packageId><modVersion>1.0.0</modVersion><supportedVersions><li>$Version</li></supportedVersions><modDependencies><li><packageId>brrainz.harmony</packageId><displayName>Harmony</displayName></li><li><packageId>CoolNether123.Spine</packageId><displayName>SpineLib</displayName></li><li><packageId>kentington.saveourship2</packageId><displayName>Save Our Ship 2</displayName></li></modDependencies><loadAfter><li>brrainz.harmony</li><li>CoolNether123.Spine</li><li>kentington.saveourship2</li></loadAfter><description>$description RimWorld $Version support build.</description></ModMetaData>
+"@;
 $loadFoldersXml = @"
 <?xml version="1.0" encoding="utf-8"?>
 <loadFolders>
